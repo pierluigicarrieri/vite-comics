22c957ee4b6c36ad9eb4048f9c6fdf12a46f0e02
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-08-22 17:35:10 +0200

    Done first component and main in App.Vue

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,9 @@
 
 <template>
   <TheHeader></TheHeader>
+  <main class="bg-dark text-white">
+    <div class="container py-5 fs-4"> -- > Content goes here --</div>
+  </main>
   <TheBanner></TheBanner>
   <TheFooter></TheFooter>
 </template>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,15 +1,38 @@
 <script>
-    export default {
-
+    export default {    
+        data() {
+                return {
+                    listitems: ["CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"],                 
+                }
+            }
 }
 </script>
 
 <template>
-
-    <header>ciaoooo</header>
-
+    <div class="container">
+        <header class="bg-warning d-flex justify-content-between">
+            <img class="my-3" src="../assets/img/dc-logo.png" alt="dc-logo">
+            <nav class="d-flex align-items-center">
+                <ul class="nav">
+                    <li class="nav-item" v-for="element in listitems">
+                        <a class="nav-link" href="#"><strong>{{element}}</strong></a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </div>
 </template>
 
 <style scoped>
+
+img {
+    aspect-ratio: 1/1;
+    height: 80px;
+}
+
+a {
+    color: #000000;
+    font-size: 0.7rem;
+}
 
 </style>
