c825ea30cf6306dba172da00f828e9c4893ddabe
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-08-22 19:13:10 +0200

    Done footer data structures prototype

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,7 +1,33 @@
 <script>
     export default {
-
-}
+    name: "TheFooter",    
+    data() {
+            return {
+                images: {
+                    backgroundImage: "",
+                    logo: ""
+                },
+                footerAreas: [
+                    {
+                        title: "DC COMICS",
+                        items: ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"]
+                    },
+                    {
+                        title: "SHOP",
+                        items: ["Shop DC", "Shop DC Collectibles"]
+                    },
+                    {
+                        title: "DC",
+                        items: ["Terms of use", "Privacy policy (New)", "Ad Choices", "Advertising", "Jobs", "Subscriptions", "Talent Workshops", "CPSC Certificates", "Ratings", "Shop Help", "Contact Us"]
+                    },
+                    {
+                        title: "SITES",
+                        items: ["DC", "MAD Magazine", "DC Kids", "DC Universe", "DC Power Visa"]
+                    },
+                ]
+            }
+        }
+    }
 </script>
 
 <template>
